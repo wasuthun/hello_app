e5ec893125516e52ccc0211a975cae84dd2b8280
Johnpaul <wasuthun.w@ku.th>
2017-04-10 08:09:14 +0000

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def hello
+      render html:"hello,world!"
+  end
+  def bye
+      render html:"Goodbye,world!"
+  end
+end
